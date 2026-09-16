72eb7170991bdcff753ec5550a6681a85d6e4f5c
swaraj-the-creator <swarajjarak@gmail.com>
2026-09-16 16:29:16 +0530

    initial commit

---
+++ Operater precedence.py
@@ -0,0 +1,15 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = (v + w) * x / y
+print("Value of (v + w) * x / y is",z)
+
+name = "Alex"
+age = 5
+
+if name == "Alex" or name == "John" and age >= 6:
+    print("Hello! Welcome")
+else:
+    print("Good Bye !!")
+
